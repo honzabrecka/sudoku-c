89fa04aae2d0091628c2ef4448da00fef8c9cb61
Jan Brecka <jan.brecka@gmail.com>
2014-10-16 11:47:59 +0200

    fix path

--- index.js
+++ index.js
@@ -6,5 +6,5 @@
 //  in accordance with the terms of the accompanying license agreement.
 // -----------------------------------------------------------------------
 
-exports = module.exports = require('build/Release/sudoku.node');
+exports = module.exports = require('./build/Release/sudoku.node');
 exports.version = require('./package').version;

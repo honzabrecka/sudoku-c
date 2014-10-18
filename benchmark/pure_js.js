3b8e2295679d85372587ad4f218f7fbf428981d6
Jan Brecka <jan.brecka@gmail.com>
2014-10-18 09:12:09 +0200

    add benchmark (and javascript implementation)

---
+++ benchmark/pure_js.js
@@ -0,0 +1,21 @@
+// -----------------------------------------------------------------------
+//  sudoku-c
+//  Copyright 2014 Jan Břečka. All Rights Reserved.
+//
+//  This program is free software. You can redistribute and/or modify it
+//  in accordance with the terms of the accompanying license agreement.
+// -----------------------------------------------------------------------
+
+var sudoku = require('./../src/sudoku_js');
+var grid = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0
+];
+var result = sudoku.solve(grid);
